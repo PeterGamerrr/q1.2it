a964fae38bcbbe40df685f3c81fa0f6f5ed58fec
petergamerrr <peterrr@assenbergh.nl>
2020-12-03 22:46:15 +0100

    removed some bs

--- app.js
+++ app.js
@@ -13,10 +13,8 @@ app.use(logger('dev'));
 //send http to https
 app.use ((req, res, next) => {
   if (req.secure) {
-    // request was via https, so do no special handling
     next();
   } else {
-    // request was via http, so redirect to https
     res.redirect('https://' + req.headers.host + req.url);
   }
 });
