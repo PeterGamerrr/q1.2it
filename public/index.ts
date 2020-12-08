d33365e2ee73584866a51c9421db6de396f24ec5
petergamerrr <hboict@vmi473245.contaboserver.net>
2020-12-08 10:40:48 +0100

    tsconf

--- public/index.ts
+++ public/index.ts
@@ -2,10 +2,17 @@ class cell{
     private x : number;
     private y : number;
     private bomb : boolean;
+    private claimedBy : number;
 
     constructor(x : number, y : number, bomb : boolean) {
         this.x = x;
         this.y = y;
         this.bomb = bomb;
     }
+
+    
+    public get x() : number {
+        return this.x;
+    }
+    
 }
