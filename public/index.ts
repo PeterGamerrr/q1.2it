73fb735533df0154cbe34d0f65fca8f0712df79f
petergamerrr <hboict@vmi473245.contaboserver.net>
2020-12-08 10:22:59 +0100

    xybomb

---
+++ public/index.ts
@@ -0,0 +1,11 @@
+class cell{
+    private x : number;
+    private y : number;
+    private bomb : boolean;
+
+    constructor(x : number, y : number, bomb : boolean) {
+        this.x = x;
+        this.y = y;
+        this.bomb = bomb;
+    }
+}
