9265fa38efbc8ff635dfc41fc3aad7840b30d8c2
petergamerrr <hboict@vmi473245.contaboserver.net>
2020-12-11 11:59:03 +0100

    menustart

--- public/index.ts
+++ public/index.ts
@@ -1,4 +1,43 @@
-class cell{
+//menu + init
+//players
+let playerCount = 2;
+function setMenuPlayerCount(num:number):void {
+    playerCount = num;
+}
+
+//size
+let menuBoardSize = 0;
+function setMenuBoardSize(num:number):void {
+    menuBoardSize = num;
+}
+
+//difficulty
+// 0: easy
+// 1: hard
+// 2: insane
+let menuDifficulty = 0;
+function setDifficulty(num:number):void {
+    menuDifficulty = num;
+}
+
+
+//actual numbers
+let boardSize:number, bombs:number;
+boardSize = 2 * playerCount + 3;
+bombs = boardSize * boardSize * (menuDifficulty * 0.05 + 0.1);
+
+//board init
+
+
+
+//game
+
+//board
+
+
+
+//board game cell
+class cell{ 
     private x: number;
     private y: number;
     private bomb: boolean;
