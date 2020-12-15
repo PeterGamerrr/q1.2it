ff14d8ee2117e33d658db300b1c24ad2fda868e9
petergamerrr <peterrr@assenbergh.nl>
2020-12-15 09:49:24 +0100

    clear func

--- public/index.js
+++ public/index.js
@@ -1,3 +1,31 @@
+//menu + init
+//players
+let playerCount = 2;
+function setMenuPlayerCount(num) {
+    playerCount = num;
+}
+//size
+let menuBoardSize = 0;
+function setMenuBoardSize(num) {
+    menuBoardSize = num;
+}
+//difficulty
+// 0: easy
+// 1: hard
+// 2: insane
+let menuDifficulty = 0;
+function setDifficulty(num) {
+    menuDifficulty = num;
+}
+//actual numbers
+let boardSize, bombs;
+boardSize = 2 * playerCount + 3;
+bombs = boardSize * boardSize * (menuDifficulty * 0.05 + 0.1);
+//board init
+$('main').empty();
+//game
+//board
+//board game cell
 class cell {
     constructor(x, y, bomb) {
         this.claimedBy = 0;
@@ -17,10 +45,3 @@ class cell {
         //TODO: make explosion
     }
 }
-cell.colors = [
-    [255, 255, 255],
-    [255, 255, 255],
-    [255, 255, 255],
-    [255, 255, 255],
-    [255, 255, 255]
-];
