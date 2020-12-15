c40e60e394823057feff49439ffc748906b6e2de
petergamerrr <peterrr@assenbergh.nl>
2020-12-15 14:46:42 +0100

    game html init

--- public/index.js
+++ public/index.js
@@ -1,13 +1,16 @@
 //menu + init
+let boardSize, bombs;
 //players
 let playerCount = 2;
 function setMenuPlayerCount(num) {
     playerCount = num;
+    console.log(num);
 }
 //size
 let menuBoardSize = 0;
 function setMenuBoardSize(num) {
     menuBoardSize = num;
+    console.log(num);
 }
 //difficulty
 // 0: easy
@@ -16,13 +19,30 @@ function setMenuBoardSize(num) {
 let menuDifficulty = 0;
 function setDifficulty(num) {
     menuDifficulty = num;
+    console.log(num);
 }
 //actual numbers
-let boardSize, bombs;
-boardSize = 2 * playerCount + 3;
-bombs = boardSize * boardSize * (menuDifficulty * 0.05 + 0.1);
 //board init
-$('main').empty();
+function startGame() {
+    boardSize = playerCount + menuBoardSize + 3;
+    bombs = boardSize * boardSize * (menuDifficulty * 0.05 + 0.1);
+    console.log("startup: " + bombs + " " + boardSize);
+    $("main").empty();
+    console.log("start");
+    $("main").load("gameboard.html", () => {
+        let gameBoard = $(".gameboard");
+        console.log(gameBoard);
+        let root = document.documentElement;
+        root.style.setProperty("--boardsize", boardSize + "");
+        for (let i = 0; i < boardSize; i++) {
+            for (let j = 0; j < boardSize; j++) {
+                let column = document.createElement("div");
+                column.classList.add("cell");
+                gameBoard.append(column);
+            }
+        }
+    });
+}
 //game
 //board
 //board game cell
--- public/index.ts
+++ public/index.ts
@@ -1,41 +1,3 @@
-//menu + init
-//players
-let playerCount = 2;
-function setMenuPlayerCount(num:number):void {
-    playerCount = num;
-}
-
-//size
-let menuBoardSize = 0;
-function setMenuBoardSize(num:number):void {
-    menuBoardSize = num;
-}
-
-//difficulty
-// 0: easy
-// 1: hard
-// 2: insane
-let menuDifficulty = 0;
-function setDifficulty(num:number):void {
-    menuDifficulty = num;
-}
-
-
-//actual numbers
-let boardSize:number, bombs:number;
-boardSize = 2 * playerCount + 3;
-bombs = boardSize * boardSize * (menuDifficulty * 0.05 + 0.1);
-
-//board init
-$('main').empty();
-
-//game
-
-
-//board
-
-
-
 //board game cell
 class cell{ 
     private x: number;
@@ -67,3 +29,64 @@ class cell{
     }
     
 }
+
+//menu + init
+let boardSize:number, bombs:number;
+
+//players
+let playerCount = 2;
+function setMenuPlayerCount(num:number):void {
+    playerCount = num;
+    console.log(num);
+}
+
+//size
+let menuBoardSize = 0;
+function setMenuBoardSize(num:number):void {
+    menuBoardSize = num;
+    console.log(num);
+}
+
+//difficulty
+// 0: easy
+// 1: hard
+// 2: insane
+let menuDifficulty = 0;
+function setDifficulty(num:number):void {
+    menuDifficulty = num;
+    console.log(num);
+}
+
+
+//actual numbers
+
+//board init
+function startGame():void {
+    boardSize = playerCount + menuBoardSize + 3 ;
+    bombs = boardSize * boardSize * (menuDifficulty * 0.05 + 0.1);
+    
+
+    console.log("startup: " + bombs + " " + boardSize)
+    $("main").empty();
+    console.log("start");
+    $("main").load("gameboard.html", () => {
+        let gameBoard = $(".gameboard");
+        console.log(gameBoard);
+        let root = document.documentElement;
+        root.style.setProperty("--boardsize",boardSize + "");
+        for (let i = 0; i < boardSize; i++) {
+            for (let j = 0; j < boardSize; j++) {
+                let column = document.createElement("div");
+                column.classList.add("cell");
+                gameBoard.append(column);
+            }
+        }
+    });
+}
+
+//game
+
+//board
+
+
+
